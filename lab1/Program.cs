8beb53b5bf7dfb6c14c999304c90a2f38321faa1
Nagnogur <56316486+Nagnogur@users.noreply.github.com>
2020-03-02 18:39:02 +0200

    Add files via upload

---
+++ lab1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Console.ReadLine();
+            Console.WriteLine(path);
+            var filePaths = Directory.GetFiles(@path, "*.csv");
+            foreach (string s in filePaths)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadKey();
+        }
+    }
+}
