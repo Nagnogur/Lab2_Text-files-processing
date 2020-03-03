04dbb5dd48670fe57cbfbfa86e826200b2f58e76
Your Name <you@example.com>
2020-03-03 15:44:50 +0200

    added class Students

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -12,11 +12,33 @@ namespace lab1
         static void Main(string[] args)
         {
             string path = Console.ReadLine();
-            Console.WriteLine(path);
             var filePaths = Directory.GetFiles(@path, "*.csv");
+
+            List<Student> rating = new List<Student>();
+            int l = 0;
             foreach (string s in filePaths)
             {
-                Console.WriteLine(s);
+                StreamReader reader = new StreamReader(File.OpenRead(@s));
+                int n = Convert.ToInt32(reader.ReadLine());
+                l += n;
+                for (int i = 0; i < n; i++)
+                {
+                    string line = reader.ReadLine();
+                    string[] values = line.Split(',');
+                    int[] grades = new int[5];
+                    for (int j = 1; j < 6; j++)
+                    {
+                        grades[j - 1] = Convert.ToInt32(values[j]);
+                    }
+                    bool budget;
+                    budget = values[6] == "FALSE" ? true : false; 
+                    rating.Add(new Student(values[0], grades , budget));
+                }
+            }
+            Student[] array = rating.ToArray();
+            for (int i = 0; i < l; i++)
+            {
+                Console.WriteLine(array[i].LastName);
             }
             Console.ReadKey();
         }
---
+++ lab1/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class Student
+    {
+        public string LastName;
+        public int[] Grades = new int[5];
+        public bool Budget;
+        public double AvarageGrade;
+        public Student(string lastName, int[] grades, bool budget)
+        {
+            LastName = lastName;
+            int sum = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                Grades[i] = grades[i];
+                sum += grades[i];
+            }
+            Budget = budget;
+            AvarageGrade = sum / 5;
+        }
+    }
+}
